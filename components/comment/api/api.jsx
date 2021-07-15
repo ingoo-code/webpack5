3d176ad288331cc543e168093270e30617cfe5f8
goak ingoo <ingoo@goakui-MacBookAir.local>
2021-07-16 03:30:04 +0900

    update api address

--- components/comment/api/api.jsx
+++ components/comment/api/api.jsx
@@ -5,7 +5,7 @@
 export const getComment = async (dispatch) => {
     dispatch({type:'GET_COMMENT'})
     try{
-        const response = await fetch('http://localhost:3000/api')
+        const response = await fetch('http://13.209.80.59/api')
         const data = await response.json()
         
         dispatch({type:'GET_COMMENT_SUCCESS',payload:data})
